1c91af91e57b7abe780e2c6ba51ece3b952d68e7
Krišjānis <65333249+chrisAS12@users.noreply.github.com>
2021-12-31 13:08:12 +0200

    Continued work on the course project

--- course-project-2021/js/main.js
+++ course-project-2021/js/main.js
@@ -1,8 +1,8 @@
 function load() {
-
-    let name = window.location.search.replace("?name=", "");
-    document.getElementById("username").innerHTML = name.charAt(0).toUpperCase() + name.slice(1);
-
+    let name = decodeURIComponent(window.location.search.replace("?name=", ""));
+    if (name != "") {
+        $(".username").html(name.charAt(0).toUpperCase() + name.slice(1) + "!");
+    }
     var navbar = document.getElementById("navbar");
     window.addEventListener("scroll", function() {
         window.scrollY > 100 ?
