41d26ad356169d0f1c0a89b7e1ed46124ae6cf33
Krišjānis <65333249+chrisAS12@users.noreply.github.com>
2022-01-01 22:01:44 +0200

    trying different stuff with form, nothing is working

--- course-project-2021/js/main.js
+++ course-project-2021/js/main.js
@@ -23,59 +23,71 @@ function changeFooterBackground() {
     }
 }
 
-function load() {
-    changeFooterBackground();
 
-    let name = decodeURIComponent(window.location.search.replace("?name=", ""));
-    if (name != "") {
-        $(".username").html(name.charAt(0).toUpperCase() + name.slice(1));
-    }
-    var navbar = document.getElementById("navbar");
-    /*
-      window.addEventListener("scroll", function() {
-          console.log(window.scrollY);
-          window.scrollY > 100 ?
-              (navbar.style.display = "block") :
-              (navbar.style.display = "none");
-      });
-      */
-    /*
-      document.body.addEventListener('scroll', function() {
-          const main = document.querySelector('#main');
+/*
+  var navbar = document.getElementById("navbar");
+ 
+    window.addEventListener("scroll", function() {
+        console.log(window.scrollY);
+        window.scrollY > 100 ?
+            (navbar.style.display = "block") :
+            (navbar.style.display = "none");
+    });
+    */
+/*
+    document.body.addEventListener('scroll', function() {
+        const main = document.querySelector('#main');
 
-          if (main.scrollTop > 50) {
-              console.log("no123");
-          } else {}
+        if (main.scrollTop > 50) {
+            console.log("no123");
+        } else {}
 
-          if (main.scrollTop > window.innerHeight / 2) {} else { console.log("hi123"); }
+        if (main.scrollTop > window.innerHeight / 2) {} else { console.log("hi123"); }
 
-      });*/
-}
+    });*/
 
+/*
 function validateForm() {
     let form = document.userForm;
-
-    function failed() {
-        return false;
-    }
-
-    if (!form.checkValidity()) {
-        return failed();
-    }
-    console.log(form);
-    if (parseInt(form["age"].value).toString().length === form["age"].value.length) {
+    console.log(parseInt(form["age"].value).toString());
+    console.log(form["age"].value);
+    if (parseInt(form["age"].value).toString() == form["age"].value) {
         alert("Age was not defined as an integer");
-        document.getElementById("age-invalid-feedback").innerHTML = "Age was not defined as an integer.";
-        return failed();
+        return false;
     } else if (form["age"].value.charAt(0) === "-") {
         alert("Age cannot be negative");
-        document.getElementById("age-invalid-feedback").innerHTML = "Age cannot be negative.";
-        return failed();
-    } else {
-        form.classList.add("was-validated");
+        return false;
     }
     return false;
 }
+
+function formValidation() {
+
+}
+*/
+
+function formSubmit(event) {
+    var url = "https://www.w3schools.com/action_page.php";
+    var request = new XMLHttpRequest();
+    request.open('get', url);
+    request.onload = function() { // request successful
+        console.log(request.responseText);
+    };
+
+    request.onerror = function() {
+        alert("Unsuccessful post.");
+    };
+
+    request.send(new FormData(event.target));
+    event.preventDefault();
+}
+
+// and you can attach form submit event like this for example
+function attachFormSubmitEvent(formId) {
+    document.getElementById(formId).addEventListener("submit", formSubmit);
+}
+
+
 /*
 (function() {
     "use strict";
@@ -109,3 +121,14 @@ function validateForm() {
     );
 })();
 */
+
+function load() {
+
+    document.getElementById("uform").addEventListener("submit", formSubmit);
+    changeFooterBackground();
+
+    let name = decodeURIComponent(window.location.search.replace("?name=", ""));
+    if (name != "") {
+        $(".username").html(name.charAt(0).toUpperCase() + name.slice(1));
+    }
+}
