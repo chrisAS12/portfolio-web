85deeb99e7053efce6763eb3c4d34510983cf2db
Krišjānis <65333249+chrisAS12@users.noreply.github.com>
2022-01-01 18:14:25 +0200

    WIP Form

--- course-project-2021/js/main.js
+++ course-project-2021/js/main.js
@@ -1,12 +1,10 @@
 function generateAffirmation() {
     $.getJSON("../data/affirmations.json", function(affirmations) {
-        let affirmation =
-            affirmations[Math.floor(Math.random() * affirmations.length)];
-        let e = document.createElement("div");
-        let p = document.createElement("p");
+        let index = Math.floor(Math.random() * affirmations.length);
+        let affirmation = affirmations[index];
+        let p = document.createElement("li");
         p.innerHTML = affirmation;
-        e.appendChild(p);
-        $(".affirmations").prepend(e);
+        $(".affirmations-list").prepend(p);
     });
 }
 
@@ -33,10 +31,45 @@ function load() {
         $(".username").html(name.charAt(0).toUpperCase() + name.slice(1));
     }
     var navbar = document.getElementById("navbar");
-    window.addEventListener("scroll", function() {
-        console.log(window.scrollY);
-        window.scrollY > 100 ?
-            (navbar.style.display = "block") :
-            (navbar.style.display = "none");
-    });
+    /*
+      window.addEventListener("scroll", function() {
+          console.log(window.scrollY);
+          window.scrollY > 100 ?
+              (navbar.style.display = "block") :
+              (navbar.style.display = "none");
+      });
+      */
+    /*
+      document.body.addEventListener('scroll', function() {
+          const main = document.querySelector('#main');
+
+          if (main.scrollTop > 50) {
+              console.log("no123");
+          } else {}
+
+          if (main.scrollTop > window.innerHeight / 2) {} else { console.log("hi123"); }
+
+      });*/
 }
+
+(function() {
+    "use strict";
+
+    var form = document.getElementById("user-form");
+
+    form.addEventListener(
+        "submit",
+        function(event) {
+            function failed() {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            if (!form.checkValidity()) {
+                failed();
+            }
+            console.log("asdasd");
+            form.classList.add("was-validated");
+        },
+        false
+    );
+})();
